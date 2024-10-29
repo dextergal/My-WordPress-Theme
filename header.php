0944de459e0d1a98bd91d3fa65e3b61cb626d47b
Dexter Gal <109516607+dextergal@users.noreply.github.com>
2024-10-30 07:35:35 +0800

    header

--- header.php
+++ header.php
@@ -8,3 +8,43 @@
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?> data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar ftco-navbar-light site-navbar-target" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="<?php echo home_url(); ?>">DEXG</a>
+	      <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav nav ml-auto">
+				<?php
+					// Check if the primary menu location has an assigned menu
+					$locations = get_nav_menu_locations();
+					$menu_id = $locations['primary']; // Replace 'primary' if using a different location name
+
+					if (isset($menu_id)) {
+						$menu_items = wp_get_nav_menu_items($menu_id);
+
+						if (!empty($menu_items)) {
+							foreach ($menu_items as $item) {
+								// Get the label (title) and classes
+								$label = $item->title;
+								$classes = implode(' ', $item->classes); // Convert classes array to string
+
+								// Output the HTML structure
+								echo '<li class="nav-item">';
+								echo '<a href="#' . esc_attr($classes) . '" class="nav-link">';
+								echo '<span>' . esc_html($label) . '</span>';
+								echo '</a>';
+								echo '</li>';
+							}
+						}
+					} else {
+						echo 'Primary menu not found.';
+					}
+				?>
+
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
